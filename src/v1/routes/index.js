19cff699a14d7e51a3dd9a4be4d2fd980501eabe
siddht1 <rt1yrru+github@gmail.com>
2024-05-24 03:32:15 +0530

    ip server

---
+++ src/v1/routes/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const cors = require('cors');
+
+// Enable CORS for all routes
+router.use(cors());
+
+const generateLogData = (req, res) => {
+  return `
+    IP: ${req.headers["x-forwarded-for"] || req.connection.remoteAddress},
+    lat: ${req.headers["x-vercel-ip-latitude"]},
+    lon: ${req.headers["x-vercel-ip-longitude"]},
+    city: ${req.headers["x-vercel-ip-city"]},
+    region: ${req.headers["x-vercel-ip-country-region"]},
+    country: ${req.headers["x-vercel-ip-country"]},
+    UA: ${req.headers["user-agent"]},
+    date_time: ${new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" })}`;
+};
+
+// Improved route handler with error handling
+router.route("/")
+  .get((req, res) => {
+    try {
+      const logData = generateLogData(req, res);
+      res.json(logData);
+    } catch (error) {
+      console.error("Error generating log data:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+module.exports = router;
